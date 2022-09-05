1eeeed111604ec00f4aec19b80cadf575cb6f36c
Pierre Sköldborg <pierre@progster.se>
2022-09-05 16:23:50 +0200

    Update lesson

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,12 +1,28 @@
 import React from "react"
-import "./Button.css"
+import styled from "styled-components"
+
+const ButtonStyled = styled.button`
+	background-color: var(--yellow);
+	border: 2px solid var(--black);
+	border-radius: 100px;
+	padding: 24px 40px;
+	text-transform: uppercase;
+	font-size: 16px;
+	font-weight: 600;
+	line-height: 24px;
+	color: var(--black);
+
+	:hover {
+		background-color: var(--yellowDark);
+	}
+`
 
 interface ButtonProps {
 	children?: React.ReactNode
 }
 
 const Button: React.FC<ButtonProps> = (props) => {
-	return <button className="button">{props.children}</button>
+	return <ButtonStyled>{props.children}</ButtonStyled>
 }
 
 export default Button
